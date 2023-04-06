da58ddc7a076233d9f4f221b867cf842e6b7029d
Sarang Gami <121340232+SarangGami@users.noreply.github.com>
2023-04-06 21:21:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ if st.button('Predict Score'):
       'balls_left': [balls_left], 'wickets_left': [wickets_left], 'crr': [crr], 'runs_last_5': [last_five_runs], 'wickets_last_5': [last_five_wickets]})
     
     result = pipe.predict(input_df)
-    st.header("Predicted Score - " + str(int(result[0]-4)) + ' TO ' + str(int(result[0]+6)))
+    st.header("Predicted Score - " + str(int(result[0])) + ' TO ' + str(int(result[0]+10)))
